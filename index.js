b75e988b394dbaae49d08c27922aa4ad0d528163
Ray Randle <randle@rays-mbp.jcc.jccal.org>
2018-02-05 16:58:01 -0600

    installed contact form

---
+++ index.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    var $contactForm = $('#contactform');
+    $contactForm.submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            url: '//formspree.io/r.randle@outlook.com',
+            method: 'POST',
+            data: $(this).serialize(),
+            dataType: 'json',
+            beforeSend: function() {
+                $contactForm.append('<div class="alert alert--loading">Sending message…</div>');
+            },
+            success: function(data) {
+                $contactForm.find('.alert--loading').hide();
+                $contactForm.append('<div class="alert alert--success">Message sent!</div>');
+            },
+            error: function(err) {
+                $contactForm.find('.alert--loading').hide();
+                $contactForm.append('<div class="alert alert--error">Ops, there was an error.</div>');
+            }
+        });
+    });
+});
